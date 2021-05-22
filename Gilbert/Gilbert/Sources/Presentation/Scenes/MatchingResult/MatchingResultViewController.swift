9196ddad947d4e7aae18bcd747b05ff8977974a7
Jaewook <hjw0911@gmail.com>
2021-05-23 04:17:10 +0900

    feat: Update MatchingResultViewController

--- Gilbert/Gilbert/Sources/Presentation/Scenes/MatchingResult/MatchingResultViewController.swift
+++ Gilbert/Gilbert/Sources/Presentation/Scenes/MatchingResult/MatchingResultViewController.swift
@@ -9,13 +9,18 @@ import UIKit
 
 class MatchingResultViewController: BaseViewController {
   
+  private var popUpCardView: UIView?
+  
   private let titleLabel = UILabel().then {
     $0.numberOfLines = 0
+    $0.textAlignment = .center
     $0.font = .font(weight: .bold, size: 16)
+    $0.text = "동행 완료!\n\n길벗과의 동행이 편안하셨나요?"
   }
   
   private let imageView = UIImageView().then {
     $0.contentMode = .scaleAspectFit
+    $0.image = UIImage(named: "temp")
   }
   
   private let grayBottomFeeLabel = UIButton().then {
@@ -34,8 +39,7 @@ class MatchingResultViewController: BaseViewController {
   
   override func viewDidLoad() {
     super.viewDidLoad()
-    
-    
+    setupUI()
   }
 }
 
@@ -55,15 +59,18 @@ extension MatchingResultViewController {
   private func layout() {
     titleLabel.snp.makeConstraints {
       $0.centerX.equalToSuperview()
-      $0.top.equalToSuperview().offset(40)
+      $0.top.equalToSuperview().offset(60)
     }
     
     imageView.snp.makeConstraints {
       $0.top.equalTo(titleLabel.snp.bottom).offset(40)
+      $0.height.equalTo(320)
+      $0.leading.equalToSuperview().offset(40)
+      $0.trailing.equalToSuperview().offset(-40)
     }
     
     bottomSendButton.snp.makeConstraints {
-      $0.bottom.equalToSuperview().offset(20)
+      $0.bottom.equalToSuperview().offset(-20)
       $0.leading.equalToSuperview().offset(24)
       $0.trailing.equalToSuperview().offset(-24)
       $0.height.equalTo(48)
@@ -74,6 +81,47 @@ extension MatchingResultViewController {
       $0.leading.equalToSuperview().offset(24)
       $0.trailing.equalToSuperview().offset(-24)
       $0.height.equalTo(32)
+    }
+  }
+  
+  private func setupPopUpCardView(gilbertInfo: Gilbert) {
+    guard popUpCardView == nil else { return }
+    
+    let backgroundView = UIView().then {
+      $0.backgroundColor = UIColor(rgb: "#6b6b6b")
+    }
+    
+    let cardView = GilbertDetailInfoCardView().then {
+      $0.configure(gilbertInfo: gilbertInfo)
+    }
+    
+    let dismissButton = UIButton().then {
+      $0.setImage(UIImage(named: "dismiss_img")?.withTintColor(.white), for: .normal)
+    }
+    
+    popUpCardView = backgroundView
+    
+    view.addSubview(backgroundView)
+    
+    backgroundView.addSubviews(
+      cardView,
+      dismissButton
+    )
+    
+    backgroundView.snp.makeConstraints {
+      $0.edges.equalToSuperview()
+    }
+    
+    cardView.snp.makeConstraints {
+      $0.width.equalTo(327)
+      $0.height.equalTo(482)
+      $0.center.equalToSuperview()
+    }
+    
+    dismissButton.snp.makeConstraints {
+      $0.width.height.equalTo(24)
+      $0.bottom.equalTo(cardView.snp.top).offset(-10)
+      $0.trailing.equalTo(cardView)
     }
   }
 }
