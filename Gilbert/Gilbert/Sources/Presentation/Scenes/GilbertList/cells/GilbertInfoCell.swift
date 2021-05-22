3461f76c2de653f2bfb085dd50a36711d4afac52
Jaewook <hjw0911@gmail.com>
2021-05-22 16:31:50 +0900

    feat: Add GilbertInfoCell

---
+++ Gilbert/Gilbert/Sources/Presentation/Scenes/GilbertList/cells/GilbertInfoCell.swift
@@ -0,0 +1,27 @@
+//
+//  GilbertInfoCell.swift
+//  Gilbert
+//
+//  Created by 황재욱 on 2021/05/22.
+//
+
+import UIKit
+
+class GilbertInfoCell: BaseCollectionViewCell {
+  
+  override init(frame: CGRect) {
+    super.init(frame: frame)
+  }
+  
+  required init?(coder: NSCoder) {
+    fatalError()
+  }
+}
+
+// MARK: - SetupUI
+extension GilbertInfoCell {
+  private func setupUI() {
+  }
+}
+
+
