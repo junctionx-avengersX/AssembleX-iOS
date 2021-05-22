5e99bd3e9354f3b421d3661cef163110dbb618c1
Jaewook <hjw0911@gmail.com>
2021-05-22 20:32:41 +0900

    feat: Add MatchingResultViewController, MatchingResultViewModel

---
+++ Gilbert/Gilbert/Sources/Presentation/Scenes/MatchingResult/MatchingResultViewController.swift
@@ -0,0 +1,29 @@
+//
+//  MatchingResultViewController.swift
+//  Gilbert
+//
+//  Created by 황재욱 on 2021/05/22.
+//
+
+import UIKit
+
+class MatchingResultViewController: BaseViewController {
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+    // Do any additional setup after loading the view.
+  }
+  
+  
+  /*
+   // MARK: - Navigation
+   
+   // In a storyboard-based application, you will often want to do a little preparation before navigation
+   override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+   // Get the new view controller using segue.destination.
+   // Pass the selected object to the new view controller.
+   }
+   */
+  
+}
