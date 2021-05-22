0a113c0212099d4db324bee3506b12e0699fcf19
Jaewook <hjw0911@gmail.com>
2021-05-22 17:22:15 +0900

    feat: Add SearchAddressViewModel

---
+++ Gilbert/Gilbert/Sources/Presentation/Scenes/SearchAddress/SearchAddressViewModel.swift
@@ -0,0 +1,27 @@
+//
+//  SearchAddressViewModel.swift
+//  Gilbert
+//
+//  Created by 황재욱 on 2021/05/22.
+//
+
+import Foundation
+
+class SearchAddressViewModel {
+  
+}
+
+extension SearchAddressViewModel {
+  
+  struct Input {
+    
+  }
+  
+  struct Output {
+    
+  }
+  
+  func transform(input: Input) -> Output {
+    Output()
+  }
+}
