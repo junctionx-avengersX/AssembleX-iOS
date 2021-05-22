a1b847d132d1478a7ce546f202f9683291d3e84f
Jaewook <hjw0911@gmail.com>
2021-05-22 16:51:44 +0900

    feat: Add SearchAddressViewController

---
+++ Gilbert/Gilbert/Sources/Presentation/Scenes/SearchAddress/SearchAddressViewController.swift
@@ -0,0 +1,17 @@
+//
+//  SearchAddressViewController.swift
+//  Gilbert
+//
+//  Created by 황재욱 on 2021/05/22.
+//
+
+import UIKit
+
+class SearchAddressViewController: BaseViewController {
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+    // Do any additional setup after loading the view.
+  }
+}
