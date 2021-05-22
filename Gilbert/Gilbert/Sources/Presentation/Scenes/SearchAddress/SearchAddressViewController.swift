5776369c50b57059c67bcbc4361dc2d3f3449cab
Jaewook <hjw0911@gmail.com>
2021-05-23 04:16:38 +0900

    feat: Update class SearchAddressViewController

--- Gilbert/Gilbert/Sources/Presentation/Scenes/SearchAddress/SearchAddressViewController.swift
+++ Gilbert/Gilbert/Sources/Presentation/Scenes/SearchAddress/SearchAddressViewController.swift
@@ -14,7 +14,7 @@ import RxDataSources
 class SearchAddressViewController: BaseViewController {
   
   private let startAddressTextField = UITextField().then {
-    
+    $0.isUserInteractionEnabled = false
     $0.backgroundColor = UIColor(rgb: "#f9f9f9")
     $0.layer.cornerRadius = 5
     $0.clearButtonMode = .always
@@ -22,10 +22,25 @@ class SearchAddressViewController: BaseViewController {
   
   private let destinationAddressTextField = UITextField().then {
     $0.layer.cornerRadius = 5
-    $0.layer.borderWidth = 1
+    $0.layer.borderWidth = 2
     $0.layer.borderColor = UIColor(rgb: "#f9f9f9").cgColor
     $0.clearButtonMode = .always
-    $0.leftView = UIImageView(image: UIImage(named: "marker_img"))
+    $0.leftViewMode = .always
+    let imageView = UIImageView(image: UIImage(named: "green_oval")).then({
+      $0.frame = CGRect(x: 20, y: 0, width: 100, height: 100)
+    })
+    $0.leftView = imageView
+  }
+  
+  private let backbuttonImageView = UIImageView().then {
+    $0.contentMode = .scaleAspectFit
+    $0.image = UIImage(named: "back_button_img")
+  }
+  
+  private let currentLocationLabel = UILabel().then {
+    $0.font = .font(weight: .regular, size: 16)
+    $0.textColor = UIColor(rgb: "#030303")
+    $0.text = "Current Location : Ori homeplus"
   }
   
   lazy var collectionView = UICollectionView(
@@ -69,6 +84,13 @@ class SearchAddressViewController: BaseViewController {
             ) else {
         return UICollectionViewCell()
       }
+      cell.backgroundButtonTap
+        .bind { [weak self] _ in
+          self?.viewModel.selectedAddressPublishRelay.accept(cellData)
+          // self?.dismiss(animated: true, completion: nil)
+      }
+      .disposed(by: cell.cellDisposeBag)
+      
       cell.configure(addressInfo: cellData)
       return cell
     } configureSupplementaryView: { sectionModel, collectionView, kind, indexPath -> UICollectionReusableView in
@@ -96,6 +118,18 @@ class SearchAddressViewController: BaseViewController {
     collectionView.snp.makeConstraints {
       $0.top.equalTo(destinationAddressTextField.snp.bottom).offset(10)
       $0.bottom.leading.trailing.equalToSuperview()
+    }
+    
+    backbuttonImageView.snp.makeConstraints {
+      $0.leading.equalToSuperview().offset(10)
+      $0.width.height.equalTo(24)
+      $0.centerY.equalToSuperview()
+    }
+    
+    currentLocationLabel.snp.makeConstraints {
+      $0.centerY.equalToSuperview()
+      $0.leading.equalTo(backbuttonImageView.snp.trailing)
+        .offset(10)
     }
   }
   
@@ -134,6 +168,11 @@ extension SearchAddressViewController {
       startAddressTextField,
       destinationAddressTextField,
       collectionView
+    )
+    
+    startAddressTextField.addSubviews(
+      backbuttonImageView,
+      currentLocationLabel
     )
   }
 }
