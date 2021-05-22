b0a60d37a2b626c05a6f4f31ab2bf76c1ec9bee5
Jaewook <hjw0911@gmail.com>
2021-05-22 19:33:38 +0900

    feat: Add base components and UIColor extension

---
+++ Gilbert/Gilbert/Sources/Presentation/Components/GreenPaddingLabel.swift
@@ -0,0 +1,20 @@
+//
+//  GreenPaddingLabel.swift
+//  Gilbert
+//
+//  Created by 황재욱 on 2021/05/22.
+//
+
+import UIKit
+
+class GreenPaddingLabel: UILabel {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
