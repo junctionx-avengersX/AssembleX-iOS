1416683a84ed2a8c3a7089096c2c3390c7a18fa4
Jaewook <hjw0911@gmail.com>
2021-05-22 23:16:49 +0900

    feat: Add models

--- Gilbert/Gilbert/Sources/Domain/Entities/AddressDetailInfo.swift
+++ Gilbert/Gilbert/Sources/Domain/Entities/AddressDetailInfo.swift
@@ -6,3 +6,35 @@
 //
 
 import Foundation
+
+struct AddressDetailInfo: Decodable {
+  let type: String?
+  let id: String
+  let title: String?
+  let x: Double?
+  let y: Double?
+  let dist: Double?
+  let totalScore: Double?
+  let sid: String?
+  let ctg: String?
+  let cid: String?
+  let jibunAddress: String?
+  let roadAddress: String?
+}
+
+/*
+{
+  "type": "place",
+  "id": "11686884",
+  "title": "서울아산병원",
+  "x": "127.1079349",
+  "y": "37.5265762",
+  "dist": 0.00024022096790207243,
+  "totalScore": 372.0712,
+  "sid": "11686884",
+  "ctg": "종합병원",
+  "cid": "223212",
+  "jibunAddress": "서울특별시 송파구 풍납2동 388-1",
+  "roadAddress": "서울특별시 송파구 올림픽로43길 88 서울아산병원"
+}
+*/
