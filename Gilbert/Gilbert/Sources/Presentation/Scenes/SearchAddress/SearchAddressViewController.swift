848450f63ec1d9584b82ddbc6e667116d4369833
Jaewook <hjw0911@gmail.com>
2021-05-22 17:22:37 +0900

    feat: Update SearchAddressViewController

--- Gilbert/Gilbert/Sources/Presentation/Scenes/SearchAddress/SearchAddressViewController.swift
+++ Gilbert/Gilbert/Sources/Presentation/Scenes/SearchAddress/SearchAddressViewController.swift
@@ -9,9 +9,50 @@ import UIKit
 
 class SearchAddressViewController: BaseViewController {
   
+  private let startAddressTextField = UITextField()
+  
+  private let destinationAddressTextField = UITextField()
+  
+  lazy var collectionView = UICollectionView(
+    frame: CGRect.zero,
+    collectionViewLayout: UICollectionViewFlowLayout().then({
+      $0.scrollDirection = .vertical
+    })
+  ).then {
+    $0.backgroundColor = .white
+    $0.showsVerticalScrollIndicator = false
+    $0.register(cellType: GilbertInfoCell.self)
+    $0.register(
+      viewType: GilbertListHeaderView.self,
+      positionType: .header
+    )
+  }
+  
+  private let viewModel: GilbertListViewModel
+  
+  // MARK: - Con(De)structor
+  
+  init(viewModel: GilbertListViewModel) {
+    self.viewModel = viewModel
+    super.init()
+  }
+  
+  required init?(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+  
   override func viewDidLoad() {
     super.viewDidLoad()
+  }
+  
+  override func setupConstraints() {
+    super.setupConstraints()
+    collectionView.snp.makeConstraints {
+      $0.bottom.leading.trailing.top.equalToSuperview()
+    }
+  }
+  
+  private func bindViewModel() {
     
-    // Do any additional setup after loading the view.
   }
 }
