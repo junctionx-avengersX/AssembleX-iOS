d36f9b69abbd1666de1356550c87bf400d700d83
Jaewook <hjw0911@gmail.com>
2021-05-22 22:50:29 +0900

    feat: Add images and DetailCardView

---
+++ Gilbert/Gilbert/Sources/Application/Extensions/UIFont+Extension.swift
@@ -0,0 +1,8 @@
+//
+//  UIFont+Extension.swift
+//  Gilbert
+//
+//  Created by 황재욱 on 2021/05/22.
+//
+
+import Foundation
---
+++ Gilbert/Gilbert/Sources/Presentation/Scenes/GilbertList/views/GilbertDetailInfoCardView.swift
@@ -0,0 +1,20 @@
+//
+//  GilbertDetailInfoCardView.swift
+//  Gilbert
+//
+//  Created by 황재욱 on 2021/05/22.
+//
+
+import UIKit
+
+class GilbertDetailInfoCardView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
